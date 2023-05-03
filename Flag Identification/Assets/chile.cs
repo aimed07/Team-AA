0428be150fb167898b4acf41e7611f46fe53f915
aimed07 <andersonhernandez2007@gmail.com>
2023-05-02 17:12:26 -0700

    tried to fix the error on the script

---
+++ Flag Identification/Assets/chile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagment;
+
+public class Level1-2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public void
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
